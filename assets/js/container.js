2dce0128199bb319a1b7a4f45350f7d434c075b4
盧信廷 Lu Bob <bob223590@gmail.com>
2024-10-22 17:13:31 +0800

    version 1

---
+++ assets/js/container.js
@@ -0,0 +1,11 @@
+$(function(){
+	$('#contents div[id !="tab1"]').hide();
+	$("a").click(function(){
+		$("#contents div").hide();
+		$($(this).attr("href")).show();
+		$(".current").removeClass("current");
+		$(this).addClass("current");
+
+		return false;
+	});
+});
